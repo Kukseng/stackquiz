e54a6f72f0f04372ade9153e802184f11636b8b3
momrotha <rothamom22@gmail.com>
2025-09-11 14:05:32 +0700

    update image signup

--- src/app/providers/StoreProvider.tsx
+++ src/app/providers/StoreProvider.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "@/store";
+import { store } from "../../lib/store";
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;

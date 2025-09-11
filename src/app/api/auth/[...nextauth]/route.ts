ee82f6f833f0bd5d178ae759d6b78497460ed3bc
Kukseng <phokukseng@gmail.com>
2025-09-11 21:44:29 +0700

    add secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import GitHubProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
 // import { signIn } from "next-auth/react";
 const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET, 
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,

93fbb2111ae5defac7ba10c0645363f50e85ce0f
Kukseng <phokukseng@gmail.com>
2025-09-11 20:17:51 +0700

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { DM_Sans } from "next/font/google";
 import LayoutWrapper from "./LayoutWrapper";
 import { LanguageProvider } from "../context/LanguageContext";
 import { Provider } from "react-redux";
-import { store } from "../lib/store"; // <-- import your store
+import { store } from "../lib/store";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const dmSans = DM_Sans({
@@ -22,9 +23,11 @@ export default function RootLayout({
     <html lang="en" className={`${dmSans.variable} antialiased`}>
       <body className="cosmic-bg overflow-hidden">
         <Provider store={store}>
-          <LanguageProvider>
-            <LayoutWrapper>{children}</LayoutWrapper>
-          </LanguageProvider>
+          <SessionProvider> 
+            <LanguageProvider>
+              <LayoutWrapper>{children}</LayoutWrapper>
+            </LanguageProvider>
+          </SessionProvider>
         </Provider>
       </body>
     </html>

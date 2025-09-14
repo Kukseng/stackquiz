d996698537a0efa76e596fd6ec3a8449fa830d61
momrotha <rothamom22@gmail.com>
2025-09-14 19:54:18 +0700

    update language

--- src/lib/api/optionApi.ts
+++ src/lib/api/optionApi.ts
@@ -1,5 +1,5 @@
 
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseApi } from "./baseApi";
 export interface Option {
   id: string;

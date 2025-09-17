ab05c4e4dde13cc3dbf36b3b47adadea50367946
EyChannim <eychannim@EyChannims-MacBook-Pro.local>
2025-09-17 17:02:00 +0700

    update dashboard organizer

--- src/components/startquiz_org/StartPage.tsx
+++ src/components/startquiz_org/StartPage.tsx
@@ -5,58 +5,9 @@ import Image from "next/image";
 import Stage from "./Stage";
 import Card from "./Card";
 import { Button } from "@/components/ui/button";
-import CardQuizComponent from "../CardQuizComponent";
 
-type CardData = {
-  id: number;
-  title: string;
-  questions: number;
-  time: string;
-  difficulty: string;
-  color: string;
-  image: string;
-};
+import ChallengeGrid from "../GridCardComponent";
 
-const quizCards: CardData[] = [
-  {
-    id: 1,
-    title: "Math Fundamental",
-    questions: 15,
-    time: "30 min",
-    difficulty: "Easy",
-    color: "bg-green-500",
-    image: "https://sohamtimes.org//wp-content/uploads/2018/07/Mathematics.png",
-  },
-  {
-    id: 2,
-    title: "Computer Programming",
-    questions: 25,
-    time: "1 hour",
-    difficulty: "Hard",
-    color: "bg-red-500",
-    image:
-      "https://media.geeksforgeeks.org/wp-content/uploads/20241017105314407132/Basics-of-Computer-Programming-For-Beginners.webp",
-  },
-  {
-    id: 3,
-    title: "Science Essentials",
-    questions: 20,
-    time: "45 min",
-    difficulty: "Medium",
-    color: "bg-yellow-500",
-    image:
-      "https://img.freepik.com/free-vector/hand-drawn-science-education-background_23-2148499325.jpg",
-  },
-  {
-    id: 4,
-    title: "Chemistry Basics",
-    questions: 15,
-    time: "30 min",
-    difficulty: "Easy",
-    color: "bg-green-500",
-    image: "https://i.ytimg.com/vi/5iTOphGnCtg/hq720.jpg",
-  },
-];
 
 export default function StartPage() {
   return (
@@ -106,21 +57,7 @@ export default function StartPage() {
         </div>
 
         {/* Bottom Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {quizCards.map((q, i) => (
-            <CardQuizComponent
-              key={q.id}
-              id={q.id}
-              index={i}
-              title={q.title}
-              questions={q.questions}
-              time={q.time}
-              difficulty={q.difficulty}
-              color={q.color}
-              image={q.image}
-            />
-          ))}
-        </div>
+        <ChallengeGrid/>
       </div>
     </Stage>
   );
